9cc651da9c16c26b295e4fea6c35cb3c980b6218
Luis Eduardo Pérez <luiseduardoperez@MacBook-Air-de-Luis.local>
2022-05-14 09:17:27 -0500

    ajuste setting.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -764,6 +764,7 @@ $settings['migrate_node_migrate_type_classic'] = false;
  * Keep this code block at the end of this file to take full effect.
  */
 #
+
 $settings['trusted_host_patterns'] = [
     '^www\.mibienestarmicompromiso\.com$',
     '^mibienestarmicompromiso\.com$',
