9eb3fc4f017bcff5dcd5108e05d2ca5ff92ff1f8
andres.alvarez <andres.alvarez@esinergia.co>
2022-05-14 02:20:07 -0500

    config  trusted host patterns

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -784,4 +784,3 @@ $settings["file_temp_path"] = "/tmp";
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
     include $app_root . '/' . $site_path . '/settings.local.php';
 }
-
