cc7feb9236ea0e486b83adfec4c82249b8671f19
andres.alvarez <andres.alvarez@esinergia.co>
2022-05-14 01:05:19 -0500

    ajustes settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -784,3 +784,4 @@ $settings['config_sync_directory'] = '../config';
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
