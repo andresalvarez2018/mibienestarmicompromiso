2d6b6034897eae549e4a135eb79d3ed808b22c89
andres.alvarez <andres.alvarez@esinergia.co>
2022-06-27 10:38:42 -0500

    ajustes modulo telefono international

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitf89dec8fee0c7849b6db8e23c9c95ac5
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitf89dec8fee0c7849b6db8e23c9c95ac5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitf89dec8fee0c7849b6db8e23c9c95ac5', 'loadClassLoader'));
